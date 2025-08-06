b4cca498e118ca31912242881335cde8b9becdf9
WEREMSOFT <pablo.weremczuk@gmail.com>
2025-08-06 08:28:56 +0200

    added more books to the stash

--- Makefile
+++ Makefile
@@ -1,2 +1,13 @@
-all:
-	gcc -g -Wall -Wextra -pedantic -std=c89 main.c
+all: build
+	./main.bin
+
+build: clean
+	gcc -g -Wall -Wextra -pedantic -std=c89 main.c -o main.bin
+
+build_release: clean
+	gcc -O3 -march=native -flto -fno-exceptions -fomit-frame-pointer -DNDEBUG -std=c89 main.c -o main.bin
+
+clean:
+	rm -f *.o
+	rm -f *.bin
+	rm -f *.out
--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
 
 #define __ARRAY_T_IMPLEMENTATION__
 #include "array_t/array_t.h"
@@ -12,21 +14,25 @@
 
 void print_words(array_t tokens, array_t token_index);
 array_t build_token_graph(array_t tokens, array_t token_index);
+void generate_phrase(array_t word_graph, char* initial_word, array_t tokens);
+node_t* find_word(char* word, array_t word_graph, array_t tokens);
 
 int main(void)
 {
 	struct stat sb;
 	char *file_content;
-	long i;
+	long i, temp_index;
 
 	bool first_char = true;
 	char null_char = 0;
+	array_t token_graph;
+
+	
 	
 	array_t tokens = array_create(1000, sizeof(char));
 	array_t token_index = array_create(1000, sizeof(long));
-	array_t token_graph = array_create(100, sizeof(node_t));
 	
-	int fd = open("libro_test.txt", O_RDONLY);
+	int fd = open("libro.txt", O_RDONLY);
 
 	if(fd == -1)
 	{
@@ -52,28 +58,78 @@ int main(void)
 	
 	for(i = 0; i < sb.st_size; i++)
 	{
-		if(file_content[i] != 32 && file_content[i] != '\n')
+		if(file_content[i] != 32 && file_content[i] != '\n' && file_content[i] != '\r' && file_content[i] != '.')
 		{
 			if(first_char)
 			{
-				array_append_element(&token_index, &i);
+				temp_index = tokens.length;
+				array_append_element(&token_index, &temp_index);
 				first_char = false;
 			}
+				
 			array_append_element(&tokens, &file_content[i]);
 		} else {
 			first_char = true;
+
+			if(file_content[i] == '.')
+			{
+				array_append_element(&tokens, &null_char);
+				array_append_element(&tokens, &file_content[i]);
+				temp_index = tokens.length - 1;
+				array_append_element(&token_index, &temp_index);
+			}
+
 			array_append_element(&tokens, &null_char);
 		}
 	}
 
-	print_words(tokens, token_index);
+	token_graph = build_token_graph(tokens, token_index);
 
-	build_token_graph(tokens, token_index);
+	generate_phrase(token_graph, "The", tokens);
+	printf("\n");
+	generate_phrase(token_graph, "If", tokens);
+	printf("\n");
+	generate_phrase(token_graph, "Then", tokens);
+	printf("\n");
+	generate_phrase(token_graph, "Although", tokens);
+	printf("\n");
+	generate_phrase(token_graph, "Finally", tokens);
 
 	munmap(file_content, sb.st_size);
 	close(fd);
 		
 	return 0;
+}
+
+void generate_phrase(array_t word_graph, char* initial_word, array_t tokens)
+{
+	long random_index, *word_index;
+	node_t* last_word_node = find_word(initial_word, word_graph, tokens);
+
+	srand((unsigned int)time(NULL));
+
+	while(last_word_node != NULL)
+	{
+		if(((char *)tokens.data)[last_word_node->token] != '.')
+		{
+			printf(" ");
+		}
+
+		printf("%s", &((char *)tokens.data)[last_word_node->token]);
+
+		if(last_word_node->children.length == 0) return;
+
+		if(((char *)tokens.data)[last_word_node->token] == '.')
+		{
+			printf("\n");
+			return;
+		}
+
+		random_index = rand() % last_word_node->children.length;
+		word_index = array_get_element_at(last_word_node->children, random_index);
+		last_word_node = find_word(&((char *)tokens.data)[*word_index], word_graph, tokens);
+	}
+	
 }
 
 void print_words(array_t tokens, array_t token_index)
@@ -85,17 +141,14 @@ void print_words(array_t tokens, array_t token_index)
 		printf("%s\n", &((char *)tokens.data)[*index]);
 	}
 
-	printf("cantidad de palabras %d\n", tokens.length);
+	printf("cantidad de palabras %d\n", token_index.length);
 }
 
-node_t find_word(char* word, array_t word_graph, array_t tokens)
+node_t* find_word(char* word, array_t word_graph, array_t tokens)
 {
 	int i;
-	node_t node = {0};
-	node_t *element;
+	node_t *element = NULL;
 	char* current_word;
-	
-	node.token = -1;
 
 	for(i = 0; i < word_graph.length; i++)
 	{
@@ -103,20 +156,27 @@ node_t find_word(char* word, array_t word_graph, array_t tokens)
 		current_word =  &((char *)tokens.data)[element->token];
 		if(strcmp(current_word, word) == 0)
 		{
-			return *element;
+			return element;
 		}
 	}
 	
-	return node;
+	return NULL;
+}
+
+node_t* word_append_to_graph(array_t* word_graph, long *index)
+{
+	node_t word_node = {0};
+	word_node = node_create();
+	word_node.token = *index;
+	array_append_element(word_graph, &word_node);
+	return array_get_element_at(*word_graph, word_graph->length - 1);
 }
 
 array_t build_token_graph(array_t tokens, array_t token_index)
 {
-	long current_word, last_word, *index;
+	long current_word, *index, last_word_index;
 	array_t word_graph = {0};
-	node_t word_node;
-	
-	last_word = -1;
+	node_t *last_word_node;
 
 	word_graph = array_create(10, sizeof(node_t));
 	
@@ -124,17 +184,23 @@ array_t build_token_graph(array_t tokens, array_t token_index)
 	{
 		index = (long *)array_get_element_at(token_index, current_word);
 
-		word_node = find_word(&((char *)tokens.data)[*index], word_graph, tokens);
-
-		if(word_node.token == -1)
+		/* add  */
+		if(current_word == 0)
 		{
-			word_node = node_create();
-			word_node.token = *index;
-			array_append_element(&word_graph, &word_node);
-			last_word = *index;
+			word_append_to_graph(&word_graph, index);
+			last_word_index = *index;
 			continue;
 		}
-		
+		/* find / add - add child  */
+		last_word_node = find_word(&((char *)tokens.data)[last_word_index], word_graph, tokens);
+
+		if(last_word_node == NULL)
+		{
+			last_word_node = word_append_to_graph(&word_graph, &last_word_index);
+		}
+
+		array_append_element(&last_word_node->children, index);
+		last_word_index = *index;
 	}
 
 	return word_graph;	
