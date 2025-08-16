f961b773a97059292fa502b6a287e5c909f0ac6d
WEREMSOFT <pablo.weremczuk@gmail.com>
2025-08-16 23:17:11 +0200

    added iteration on token datastructure

--- main.c
+++ main.c
@@ -14,10 +14,11 @@
 
 void print_words(array_t tokens, array_t token_index);
 array_t build_token_graph(array_t tokens, array_t token_index);
-void generate_phrase(array_t word_graph, char* initial_word, array_t tokens);
-node_t* find_word(char* word, array_t word_graph, array_t tokens);
+void generate_phrase(array_t word_graph, char** initial_word, array_t tokens);
+node_t* find_word(char** word, array_t word_graph, array_t tokens);
 void generate_tokens(array_t* tokens, array_t* token_index);
 void print_graph(array_t graph, array_t tokens);
+void generate_texts(array_t token_graph, array_t tokens);
 
 int main(void)
 {
@@ -52,17 +53,22 @@ int main(void)
 	{
 		token_graph = build_token_graph(tokens, token_index);
 	}
-
-	printf("\n");
-	generate_phrase(token_graph, "If", tokens);
-	printf("\n");
-	generate_phrase(token_graph, "Then", tokens);
-	printf("\n");
-	generate_phrase(token_graph, "Although", tokens);
-	printf("\n");
-	generate_phrase(token_graph, "Finally", tokens);
+	generate_texts(token_graph, tokens);
 	
 	return 0;
+}
+
+void generate_texts(array_t token_graph, array_t tokens)
+{
+	printf("\n");
+	char *phrase_initial_words[] = {"If", "", ""};
+	generate_phrase(token_graph, phrase_initial_words, tokens);
+	printf("\n");
+	generate_phrase(token_graph, phrase_initial_words, tokens);
+	printf("\n");
+	generate_phrase(token_graph, phrase_initial_words, tokens);
+	printf("\n");
+	generate_phrase(token_graph, phrase_initial_words, tokens);
 }
 
 void print_graph(array_t graph, array_t tokens)
@@ -153,7 +159,7 @@ void generate_tokens(array_t* tokens, array_t* token_index)
 		close(fd);
 }
 
-void generate_phrase(array_t word_graph, char* initial_word, array_t tokens)
+void generate_phrase(array_t word_graph, char** initial_word, array_t tokens)
 {
 	long random_index, *word_index;
 	node_t* last_word_node = find_word(initial_word, word_graph, tokens);
@@ -195,7 +201,7 @@ void print_words(array_t tokens, array_t token_index)
 	printf("cantidad de palabras %ld\n", token_index.length);
 }
 
-node_t* find_word(char* word, array_t word_graph, array_t tokens)
+node_t* find_word(char** word, array_t word_graph, array_t tokens)
 {
 	int i;
 	node_t *element = NULL;
@@ -205,7 +211,7 @@ node_t* find_word(char* word, array_t word_graph, array_t tokens)
 	{
 		element = (node_t *)array_get_element_at(word_graph, i);
 		current_word =  &((char *)tokens.data)[element->token[0]];
-		if(strcmp(current_word, word) == 0)
+		if(strcmp(current_word, word[0]) == 0)
 		{
 			return element;
 		}
@@ -216,9 +222,15 @@ node_t* find_word(char* word, array_t word_graph, array_t tokens)
 
 node_t* word_append_to_graph(array_t* word_graph, long *index)
 {
+	int param_counter;
 	node_t word_node = {0};
 	word_node = node_create();
-	word_node.token[0] = index[0];
+
+	for(param_counter = 0; param_counter < NODE_NUM_PARAMS; param_counter++)
+	{
+		word_node.token[param_counter] = index[param_counter];
+	}
+	
 	array_append_element(word_graph, &word_node);
 	return array_get_element_at(*word_graph, word_graph->length - 1);
 }
